dc24ef53a72c3aa8a0e36dfe014de8e1421cc15e
DevLop-EC <christlopbsc12@gmail.com>
2022-07-01 14:42:30 -0500

    Consuming countries api and install library for this

--- funiber-client/src/components/FuniberProgramsForm/FuniberProgramForms.jsx
+++ funiber-client/src/components/FuniberProgramsForm/FuniberProgramForms.jsx
@@ -1,33 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import csc from 'country-state-city';
+import Select from 'react-select';
+
+import { useFormik } from 'formik';
 
 export const FuniberProgramForms = () => {
+  const addressFromik = useFormik({
+    initialValues: {
+      country: 'India',
+      state: null,
+      city: null,
+    },
+    onSubmit: (values) => console.log(values),
+  });
+
+  const countries = csc.getAllCountries();
+
+  const updatedCountries = countries.map((country) => ({
+    label: country.name,
+    value: country.isoCode,
+    ...country,
+  }));
+  const updatedStates = (countryId) =>
+    csc
+      .getStatesOfCountry(countryId)
+      .map((state) => ({ label: state.name, value: state.isoCode, ...state }));
+  const updatedCities = (countryCode, stateId) =>
+    csc
+      .getCitiesOfState(countryCode, stateId)
+      .map((city) => ({ label: city.name, value: city.stateCode, ...city }));
+
+  const { values, handleSubmit, setFieldValue, setValues } = addressFromik;
+
+  useEffect(() => {
+    console.log({ values });
+  }, [values]);
+
   return (
-    <div class="mb-4 p-2 mx-8 sm:mt-0">
-      <div class="md:grid md:grid-cols-3 md:gap-6">
-        <div class="md:col-span-1">
-          <div class="px-4 sm:px-0">
-            <h3 class="text-lg font-medium leading-6 text-blue-500">
+    <div className="mb-4 p-2 mx-8 sm:mt-0">
+      <div className="md:grid md:grid-cols-3 md:gap-6">
+        <div className="md:col-span-1">
+          <div className="px-4 sm:px-0">
+            <h3 className="text-lg font-medium leading-6 text-blue-500">
               Request Information
             </h3>
           </div>
         </div>
-        <div class="mt-5 md:mt-0 md:col-span-3">
-          <form action="#" method="POST">
-            <div class="shadow overflow-hidden sm:rounded-md">
-              <div class="px-4 py-5 bg-white sm:p-6">
-                <div class="grid grid-cols-6 gap-6">
-                  <div class="col-span-6 sm:col-span-3">
+        <div className="mt-5 md:mt-0 md:col-span-3">
+          <form onSubmit={handleSubmit}>
+            <div className="shadow overflow-hidden sm:rounded-md">
+              <div className="px-4 py-5 bg-white sm:p-6">
+                <div className="grid grid-cols-6 gap-6">
+                  <div className="col-span-6 sm:col-span-3">
                     <label
-                      for="country"
-                      class="block text-sm font-medium text-gray-700"
+                      htmlFor="country"
+                      className="block text-sm font-medium text-gray-700"
                     >
                       Knowledge Areas
                     </label>
                     <select
                       id="country"
                       name="country"
-                      autocomplete="country"
-                      class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     >
                       <option>United States</option>
                       <option>Canada</option>
@@ -35,18 +69,17 @@ export const FuniberProgramForms = () => {
                     </select>
                   </div>
 
-                  <div class="col-span-6 sm:col-span-3">
+                  <div className="col-span-6 sm:col-span-3">
                     <label
-                      for="country"
-                      class="block text-sm font-medium text-gray-700"
+                      htmlFor="country"
+                      className="block text-sm font-medium text-gray-700"
                     >
                       Program
                     </label>
                     <select
                       id="country"
                       name="country"
-                      autocomplete="country"
-                      class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     >
                       <option>United States</option>
                       <option>Canada</option>
@@ -54,10 +87,10 @@ export const FuniberProgramForms = () => {
                     </select>
                   </div>
 
-                  <div class="col-span-6 sm:col-span-3">
+                  <div className="col-span-6 sm:col-span-3">
                     <label
-                      for="email_address"
-                      class="block text-sm font-medium text-gray-700 "
+                      htmlFor="email_address"
+                      className="block text-sm font-medium text-gray-700 "
                     >
                       Names
                     </label>
@@ -65,15 +98,14 @@ export const FuniberProgramForms = () => {
                       type="text"
                       name="first_name"
                       id="first_name"
-                      autocomplete="given-name"
-                      class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
                   </div>
 
-                  <div class="col-span-6 sm:col-span-3">
+                  <div className="col-span-6 sm:col-span-3">
                     <label
-                      for="country"
-                      class="block text-sm font-medium text-gray-700"
+                      htmlFor="country"
+                      className="block text-sm font-medium text-gray-700"
                     >
                       Last names
                     </label>
@@ -81,14 +113,13 @@ export const FuniberProgramForms = () => {
                       type="text"
                       name="first_name"
                       id="first_name"
-                      autocomplete="given-name"
-                      class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
                   </div>
-                  <div class="col-span-6 sm:col-span-6 lg:col-span-3">
+                  <div className="col-span-6 sm:col-span-6 lg:col-span-3">
                     <label
-                      for="street_address"
-                      class="block text-sm font-medium text-gray-700"
+                      htmlFor="street_address"
+                      className="block text-sm font-medium text-gray-700"
                     >
                       Email
                     </label>
@@ -96,15 +127,14 @@ export const FuniberProgramForms = () => {
                       type="email"
                       name="email"
                       id="email"
-                      autocomplete="given-name"
-                      class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
                   </div>
 
-                  <div class="col-span-6 sm:col-span-6 lg:col-span-3">
+                  <div className="col-span-6 sm:col-span-6 lg:col-span-3">
                     <label
-                      for="city"
-                      class="block text-sm font-medium text-gray-700"
+                      htmlFor="city"
+                      className="block text-sm font-medium text-gray-700"
                     >
                       Phone
                     </label>
@@ -112,100 +142,106 @@ export const FuniberProgramForms = () => {
                       type="number"
                       name="phone"
                       id="phone"
-                      autocomplete="given-name"
-                      class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     />
                   </div>
 
-                  <div class="col-span-6 sm:col-span-3 lg:col-span-3">
+                  <div className="col-span-6 sm:col-span-3 lg:col-span-3">
                     <label
-                      for="state"
-                      class="block text-sm font-medium text-gray-700"
+                      htmlFor="state"
+                      className="block text-sm font-medium text-gray-700"
                     >
                       Country of residence
                     </label>
-                    <select
+                    <Select
                       id="country"
                       name="country"
-                      autocomplete="country"
-                      class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    >
-                      <option>United States</option>
-                      <option>Canada</option>
-                      <option>Mexico</option>
-                    </select>
+                      label="country"
+                      options={updatedCountries}
+                      value={values.country}
+                      onChange={(value) => {
+                        setValues(
+                          { country: value, state: null, city: null },
+                          false,
+                        );
+                      }}
+                      className="mt-1 block w-full py-2 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
                   </div>
 
-                  <div class="col-span-6 sm:col-span-3 lg:col-span-3">
+                  <div className="col-span-6 sm:col-span-3 lg:col-span-3">
                     <label
-                      for="postal_code"
-                      class="block text-sm font-medium text-gray-700"
+                      htmlFor="postal_code"
+                      className="block text-sm font-medium text-gray-700"
                     >
                       State
                     </label>
-                    <select
-                      id="country"
-                      name="country"
-                      autocomplete="country"
-                      class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    >
-                      <option>United States</option>
-                      <option>Canada</option>
-                      <option>Mexico</option>
-                    </select>
+                    <Select
+                      id="state"
+                      name="state"
+                      options={updatedStates(
+                        values.country ? values.country.value : null,
+                      )}
+                      value={values.state}
+                      onChange={(value) => {
+                        setValues({ state: value, city: null }, false);
+                      }}
+                      className="mt-1 block w-full py-2  bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
                   </div>
-                  <div class="col-span-6 sm:col-span-3 lg:col-span-3">
+                  <div className="col-span-6 sm:col-span-3 lg:col-span-3">
                     <label
-                      for="postal_code"
-                      class="block text-sm font-medium text-gray-700"
+                      htmlFor="postal_code"
+                      className="block text-sm font-medium text-gray-700"
                     >
                       City
                     </label>
-                    <select
-                      id="country"
-                      name="country"
-                      autocomplete="country"
-                      class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    >
-                      <option>United States</option>
-                      <option>Canada</option>
-                      <option>Mexico</option>
-                    </select>
+                    <Select
+                      id="city"
+                      name="city"
+                      options={updatedCities(
+                        values.state?.countryCode,
+                        values.state?.value,
+                      )}
+                      value={values.city}
+                      onChange={(value) => setFieldValue('city', value)}
+                      className="mt-1 block w-full py-2  bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
                   </div>
-                  <div class="col-span-6 sm:col-span-3 lg:col-span-3">
+                  <div className="col-span-6 sm:col-span-3 lg:col-span-3">
                     <label
-                      for="postal_code"
-                      class="block text-sm font-medium text-gray-700 p-1"
+                      htmlFor="postal_code"
+                      className="block text-sm font-medium text-gray-700 p-1"
                     >
                       Comments
                     </label>
                     <textarea
                       id="message"
                       rows="4"
-                      class="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500    dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500    dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="Your message..."
                     ></textarea>
                   </div>
                 </div>
               </div>
 
-              <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                <div class="flex items-center p-2">
+              <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                <div className="flex items-center p-2">
                   <input
-                    checked
+                    defaultChecked={true}
                     id="checkbox-1"
                     type="checkbox"
                     value=""
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
                   <label
-                    for="checkbox-1"
-                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    htmlFor="checkbox-1"
+                    className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                   >
                     I accept the{' '}
                     <a
                       href="#"
-                      class="text-blue-600 hover:underline dark:text-blue-500"
+                      className="text-blue-600 hover:underline dark:text-blue-500"
                     >
                       the privacy policy
                     </a>
@@ -214,7 +250,7 @@ export const FuniberProgramForms = () => {
                 </div>
                 <button
                   type="submit"
-                  class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Send
                 </button>
--- funiber-client/src/components/Navbar/Navbar.jsx
+++ funiber-client/src/components/Navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import { ApplicationLogo } from '../ApplicationLogo/ApplicationLogo';
 export const Navbar = () => {
   return (
     <header>
-      <div class="bg-blue-500">
-        <div class="w-full px-2 py-4 mx-auto sm:px-6 lg:px-4">
-          <div class="sm:flex sm:items-center sm:justify-between"></div>
+      <div className="bg-blue-500">
+        <div className="w-full px-2 py-4 mx-auto sm:px-6 lg:px-4">
+          <div className="sm:flex sm:items-center sm:justify-between"></div>
         </div>
       </div>
-      <div class="max-w-screen-xl px-4 mx-auto sm:py-1 sm:px-4 lg:px-2">
-        <div class="sm:justify-between sm:items-center sm:flex">
-          <div class="text-center sm:text-left">
+      <div className="max-w-screen-xl px-4 mx-auto sm:py-1 sm:px-4 lg:px-2">
+        <div className="sm:justify-between sm:items-center sm:flex">
+          <div className="text-center sm:text-left">
             <ApplicationLogo />
           </div>
         </div>
