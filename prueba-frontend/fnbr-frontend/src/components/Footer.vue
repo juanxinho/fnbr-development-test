cec30d91c689ad83635ade7a7720faa93115f266
MiguelBohorquezLopez98 <dlsg1009@gmail.com>
2024-01-04 21:52:03 -0500

    Agregando frontend

---
+++ prueba-frontend/fnbr-frontend/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="footer">
+        <div class="w-full bg-gray-400 mt-16">
+            <div class="container flex flex-col mx-auto">
+                <div class="flex flex-col items-start w-full py-5">
+                    <div class="flex">
+                        <p class="text-base font-normal leading-7 text-center text-white">©
+                            Copyright 2005 - 2022. Ibero-American University Fundation - All rights reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+};
+</script>
---
+++ prueba-frontend/fnbr-frontend/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header">
+        <div class="flex flex-col mx-auto w-full">
+            <div class="relative flex flex-wrap items-center justify-between w-full bg-sky-800 group py-5 shrink-0 ">
+            </div>
+            <div class="relative flex flex-wrap items-center justify-between w-full group py-5 shrink-0 border-b-2">
+                <div class="pl-10">
+                    <a href="/">
+                        <img class="h-24 w-full" src="/imgs/logo-funiber.png" title="Funiber">
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+};
+</script>
